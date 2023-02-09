f17f8949611f96d3a58e012781e6d62930f6e236
jexzas <jdrmail2007@yahoo.com>
2023-02-09 16:45:45 -0600

    stuff

---
+++ Chapter 3/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+
+int main () {
+    
+    double dec;
+    string str;
+    int num;
+    
+    cin >> dec >> str >> num;
+
+    cout << str << " " << dec << " " << num << endl;
+}
